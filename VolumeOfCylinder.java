4943bf9bef7349055844359e5aa2bbbaca9b7631
Popoola Tunde Malik <popoolatunde53@gmail.com>
2021-02-19 03:24:25 +0100

    java

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class VolumeOfCylinder {
+    public static void main(String... args){
+        double  radius;
+        double  area;
+        double length;
+        double volume;
+        double pi = Math.PI;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the length");
+        length = input.nextDouble();
+
+        System.out.println("Enter the radius");
+        radius = input.nextDouble();
+
+        area =  radius * radius * pi;
+        System.out.println("The area of the cylinder is: " + area);
+
+        volume = area * length;
+        System.out.println("The volume of cylinder is: " + volume);
+
+    }
+}
